5daf333d53575b1e5a5083f9a3834f41d6ef837c
ElizPN <pankevych.liza@gmail.com>
2022-07-14 12:54:26 +0200

    add post list

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const PostsList = () => {
+  const posts = useSelector((state) => state.posts)
+  console.log(posts)
+
+  const renderedPost = posts.map((post) => (
+    <article className='"post-excerpt' key={post.id}>
+      <h3>{post.title}</h3>
+      <p className='"post-content'>{post.content.substring(0, 100)}</p>
+    </article>
+  ))
+
+  return (
+    <section className="post-list">
+      <h2>Posts</h2>
+      {renderedPost}
+    </section>
+  )
+}
